0718a4d2a12cb61e0e8a1269fc2228d2fe1b4624
peillis <enrique@markhaus.com>
2017-04-25 22:17:00 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elasticachex.Mixfile do
   def project do
     [
       app: :elasticachex,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
