3b7a0678adf80d748dfb303d7ce83c7fafdb1565
peillis <enrique@doofinder.com>
2017-12-02 13:47:50 +0100

    test for multiple nodes

--- test/elasticachex_test.exs
+++ test/elasticachex_test.exs
@@ -11,4 +11,9 @@ defmodule ElasticachexTest do
     assert Elasticachex.get_cluster_info("version<1.4.14") ==
       {:ok, ["10.0.9.238:11211"], "1"}
   end
+
+  test "returns multiple nodes" do
+    assert Elasticachex.get_cluster_info("multiple_nodes") ==
+      {:ok, ["10.80.249.27:11211", "10.82.235.120:11211"], "12"}
+  end
 end
--- test/support/socket.ex
+++ test/support/socket.ex
@@ -6,6 +6,9 @@ defmodule Elasticachex.SocketTest do
   def connect("version<1.4.14", _port, _timeout) do
     {:ok, :socket_2}
   end
+  def connect("multiple_nodes", _port, _timeout) do
+    {:ok, :multiple_nodes}
+  end
 
   def send_and_recv(:socket_1, command, _timeout) do
     case command do
@@ -17,6 +20,12 @@ defmodule Elasticachex.SocketTest do
     case command do
       "version\n" -> {:ok, "VERSION 1.4.13\r\n"}
       "get AmazonElastiCache:cluster\n" -> {:ok, "CONFIG cluster 0 74\r\n1\nmemcached-cluster.xlttkm.0001.euw1.cache.amazonaws.com|10.0.9.238|11211\n\r\nEND\r\n"}
+    end
+  end
+  def send_and_recv(:multiple_nodes, command, _timeout) do
+    case command do
+      "version\n" -> {:ok, "VERSION 1.4.14\r\n"}
+      "config get cluster\n" -> {:ok, "CONFIG cluster 0 147\r\n12\nmyCluster.pc4ldq.0001.use1.cache.amazonaws.com|10.82.235.120|11211 myCluster.pc4ldq.0002.use1.cache.amazonaws.com|10.80.249.27|11211\n\r\nEND\r\n"}
     end
   end
 
