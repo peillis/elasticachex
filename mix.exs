c281811b5c5506eef26b8141ae44ddc5098c2d27
peillis <enrique@doofinder.com>
2018-08-16 09:15:52 +0200

    deps update

--- mix.exs
+++ mix.exs
@@ -41,8 +41,8 @@ defmodule Elasticachex.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:credo, "~> 0.8", only: :dev},
-      {:ex_doc, "~> 0.18", only: :dev},
+      {:credo, "~> 0.10", only: :dev},
+      {:ex_doc, "~> 0.19", only: :dev},
       {:socket, "~> 0.3"}
     ]
   end
