bd3c0751a4ad4f1739e87b0efc6faf9c8652018c
peillis <enrique@doofinder.com>
2018-08-16 09:05:49 +0200

    bump version and update socket

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elasticachex.Mixfile do
   def project do
     [
       app: :elasticachex,
-      version: "1.1.1",
+      version: "1.1.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
